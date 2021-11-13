afebef9fce2fae3f3e27bfd50927178598ad5802
bgu152 <erikrammschmidt@gmail.com>
2021-11-13 11:31:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [task,setTask] = React.useState({desc:'', date:''});
+  const [todos,setTodos] = React.useState([]);
+
+  const inputChanged =(event) =>{
+    setTask({...task, [event.target.id]:event.target.value});
+  }
+  
+  const addTodo =(event) => {
+    event.preventDefault();
+    setTodos([...todos, task]);
+  }
+
+  const deleteRow = (event) => {
+    alert(event.target.id);
+    let newTodos= todos.filter((todo, i) => i != event.target.id);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todolist</h1>
       </header>
+
+      <fieldset> 
+        <legend align="left">Add todo</legend>
+      
+      <form onSubmit={addTodo}>
+        <label for="task">Task</label>
+        <input type="text" onChange={inputChanged} id="desc"/>
+        <label for="date">Date</label>
+        <input type="text" onChange={inputChanged} id="date"/>
+        <input type="submit" value="Add"/>
+      </form>
+      </fieldset>
+
+
+
+      <table>
+        <tbody>
+          
+          <tr><th>Date</th><th>Description</th></tr>
+          {
+            todos.map((todo,index) => 
+            <tr key={index}>
+              <td>{todo.date}</td>
+              <td>{todo.desc}</td>
+              <td><input type="button" value="Delete" id={index} onClick={deleteRow}/></td>
+            </tr>
+              )
+          }
+        </tbody>
+      </table>  
     </div>
   );
 }
